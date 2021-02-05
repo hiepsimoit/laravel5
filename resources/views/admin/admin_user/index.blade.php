9a8dd24de2783fe16973f8b1475463b8061cd596
Dao Thanh Tung <tungdt@TungDT-2.local>
2021-02-05 13:32:50 +0700

    update icon button

--- resources/views/admin/admin_user/index.blade.php
+++ resources/views/admin/admin_user/index.blade.php
@@ -17,12 +17,11 @@
                     <td> {{ $i }} </td>
                     <td> {{ $list->username }} </td>
                     <td>
-                        <a href="{{ route('admin_user.edit',$list->id)}}" class="btn btn-primary">Edit</a>
-                        <form action="{{route('admin_user.destroy',$list->id)}}" method="post">
+                        <a href="{{ route('admin_user.edit',$list->id)}}" class="btn btn-primary pull-left" style="margin-right: 10px"><span class="fa fa-edit "></span></a>
+                        <form class="pull-left" action="{{route('admin_user.destroy',$list->id)}}" method="post">
                             <input type="hidden" name="_token" value="{{csrf_token()}}">
                             <input type="hidden" name="_method" value="delete">
-                            <button class="btn btn-sm btn-danger rounded-0">
-                                Delete
+                            <button class="btn btn-danger "><span class="fa fa-remove "></span>
                             </button>
                         </form>
                     </td>
